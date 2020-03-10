6df608f95405e8ba3ad1368cdd9512dd3332012f
mhexsel <marcelo.hexsel@hotmail.com>
2020-03-10 18:14:22 -0300

    arrays

---
+++ arrays.js
@@ -0,0 +1,38 @@
+let array = ["A", "B", "C", "D", "E"];
+
+//percorrendo um array com programação funcional (arrow function)
+array.forEach(letra => {
+  console.log(letra);
+});
+
+//Filtrando um array para buscar elementos a partir da letra 'D'
+let letrasAPartirDoD = array.filter(letra => {
+  return letra >= "D";
+});
+console.log(letrasAPartirDoD);
+
+//Arrays podem conter objetos!
+let arrayDeObjetos = [
+  {
+    nome: "Fulano",
+    idade: 15
+  },
+  {
+    nome: "Ciclano",
+    idade: 20
+  },
+  {
+    nome: "Beltrano",
+    idade: 21
+  },
+  {
+    nome: "Joana",
+    idade: 80
+  }
+];
+
+//Filtrando arrays de objetos, apenas idade menor igual a 20
+let pessoasCom20AnosOuMenos = arrayDeObjetos.filter(pessoa => {
+  return pessoa.idade <= 20;
+});
+console.log(pessoasCom20AnosOuMenos);
